a3875a23749e6d5fd160be51bffcab7ccbf6315e
Josiah <jwahlrab@gmail.com>
2015-02-24 19:32:32 -0500

    Minor change to findAndCopyScript in Exploration

--- Homework/explorePython/findAndCopyFiles.py
+++ Homework/explorePython/findAndCopyFiles.py
@@ -5,12 +5,6 @@ import glob, os, shutil
 source_dir = "/Users/josiah_MBP/Desktop/sourceFolder/"
 dest_dir = "/Users/josiah_MBP/Desktop/destinationFolder/"
 
-# files = glob.iglob(os.path.join(source_dir, "*.txt"))
-# for file in files:
-#     if os.path.isfile(file):
-#         shutil.copy2(file, dest_dir)
-
-toCopyList = []
 for root, dirs, files in os.walk(source_dir):
     for name in files:
         if name.endswith(".png"):
